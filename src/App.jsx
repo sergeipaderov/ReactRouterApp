523a67f5c823e5544298ee3533115bbb8c7d6506
sergeipaderov <sergeipaderov@gmail.com>
2017-07-08 12:02:11 -0300

    last  coommit

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,50 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { Route, Switch, withRouter } from 'react-router-dom';
 
 import Toolbar from './components/Toolbar';
 import Content from './components/Content';
+import Sidenav from './components/Sidenav';
+import PrivateRoute from './components/PrivateRoute';
 import Home from './pages/Home';
 import About from './pages/About';
 import Books from './pages/Books';
 import Login from './pages/Login';
+import Logout from './pages/Logout';
+import NotFound from './pages/NotFound';
 
 class App extends Component {
+    state = {
+        user: null
+    }
+
+    login = user => {
+        this.setState({ user }, () => this.props.history.push('/books'));
+    }
+
+    logout = () => {
+        this.setState({ user: null }, () => this.props.history.push('/'));
+    }
+
     render() {
-        return (
-            <Router>
-                <div className="app">
-                    <Toolbar />
-                    
-                    <Content>
+        return (            
+            <div className="app">
+                <Toolbar user={this.state.user} />
+                
+                <Content>
+                <Route path="/books" render={() => <Sidenav topics={this.props.topics} />} />
+                    <Switch>
                         <Route exact path="/" component={Home} />
-                        <Route path="/about" component={About} />
-                        <Route path="/books" component={Books} />
-                        <Route path="/login" component={Login} />
-                    </Content>
-                </div>
-            </Router>
+                        <Route path="/about" component={About} />                            
+                        <Route path="/login" render={props => <Login onLogin={this.login} />} />
+                        <Route path="/logout" render={props => <Logout onLogin={this.logout} />} />
+                        <PrivateRoute exact path="/books/:topic?" user={this.state.user} component={Books} data={this.props.books} />
+                        <privateRoute path="/books/:topic/:book" user={this.state.user} component={Books} data={this.props.books} />
+                        <Route component={NotFound} />
+                    </Switch>
+                </Content>
+            </div>            
         );
     }
 }
 
-export default App;
+export default withRouter(App);
--- src/components/Sidenav.jsx
+++ src/components/Sidenav.jsx
@@ -1,19 +1,20 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 
-import topics from '../data/topics';
-
-function Sidenav() {
+function Sidenav({ topics }) {
     return (
         <aside className="mdc-permanent-drawer">
             <nav className="mdc-list mdc-list--avatar-list">
                 {topics.map(topic => 
-                    <a
+                    <NavLink
                         key={topic.id}
-                        href={`/books/${topic.id}`}
-                        className="mdc-list-item">
-                        <img src={`/img/topics/${topic.id}.svg`} className="mdc-list-item__start-detail"/>
+                        to={`/books/${topic.id}`}
+                        className="mdc-list-item"
+                        activeClassName="mdc-temporary-drawer--selected">
+                        <img src={`/img/topics/${topic.id}.svg`} 
+                        className="mdc-list-item__start-detail" alt="book"/>
                             {topic.title}
-                    </a>
+                    </NavLink>
                 )}
             </nav>
         </aside>
